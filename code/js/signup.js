d3303d5aa8baab6338db9043b25847dcf83114e2
Lee Seung Hoon <tmdgns6475@naver.com>
2021-08-27 00:34:12 +0900

    20210827

---
+++ code/js/signup.js
@@ -0,0 +1,5 @@
+const animate = () => {
+    if(document.getElementsByTagName('input').onclick == true) {
+        document.getElementsByTagName('input').style.borderBottom = "3px solid #307FE2";
+    }
+}
