6bef50e6e6f03de42a76990c0df4000523e4e08e
John Doe <swh45914255@gmail.com>
2021-09-28 11:50:24 +0900

    서버쪽만 하면 됨

--- code/script.js
+++ code/script.js
@@ -1,7 +1,6 @@
-function test(){
-    console.log('lele');
-}
-
-function signWithGoogle(){
-    console.log("you clicked sign with google   ")
+const LoginBtn = () => {
+    const ID = document.getElementById('ID').value;
+    const PW = document.getElementById('PW').value;
+    console.log('로그인 버튼 클릭');
+    alert(`아이디는 ${ID}, 비밀번호는 ${PW}입니다.`)
 }
